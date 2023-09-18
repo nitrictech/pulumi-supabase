45208134fef60a334528b2c50859443873d7ebbf
Tim Holm <tim.holm@nitric.io>
2023-09-19 08:47:25 +1000

    move migrations to v0 API.

--- provider/pkg/provider/supabase/v0/migration.go
+++ provider/pkg/provider/supabase/v0/migration.go
@@ -1,4 +1,4 @@
-package v1
+package v0
 
 import (
 	"bytes"
@@ -23,7 +23,7 @@ type Migration struct{}
 // Each resource has in input struct, defining what arguments it accepts.
 type MigrationArgs struct {
 	// ProjectId - The remote host to apply the migration to
-	ProjectId string `pulumi:"project_id,optional"`
+	ProjectRef string `pulumi:"project_ref,optional"`
 	// DbPassword - The database password
 	DbPassword string `pulumi:"db_password"`
 	// IncludeAll - Include all migrations not found on remote history table.
@@ -51,25 +51,13 @@ func (Migration) Create(ctx p.Context, name string, input MigrationArgs, preview
 		return name, state, fmt.Errorf("supabase CLI not installed, required to push remote migrations")
 	}
 
-	// linkCmd := exec.Command(
-	// 	"supabase",
-	// 	"link",
-	// 	"--project-ref",
-	// 	input.ProjectId,
-	// )
-
-	// err := linkCmd.Run()
-	// if err != nil {
-	// 	return name, state, fmt.Errorf("failed to link supabase project %s: %w", input.ProjectId, err)
-	// }
-
 	extraFlags := []string{
 		"db", "push", "-p", input.DbPassword,
 	}
 
-	if input.ProjectId != "" {
+	if input.ProjectRef != "" {
 		extraFlags = append(extraFlags, "--db-url")
-		extraFlags = append(extraFlags, fmt.Sprintf("postgresql://postgres:%s@db.%s.supabase.co:5432/postgres", input.DbPassword, input.ProjectId))
+		extraFlags = append(extraFlags, fmt.Sprintf("postgresql://postgres:%s@db.%s.supabase.co:5432/postgres", input.DbPassword, input.ProjectRef))
 	}
 
 	if input.IncludeAll {
@@ -96,7 +84,7 @@ func (Migration) Create(ctx p.Context, name string, input MigrationArgs, preview
 
 	err := migrateCmd.Run()
 	if err != nil {
-		return name, state, fmt.Errorf("failed to apply migrations to project %s: %s, %s", input.ProjectId, outb.String(), errb.String())
+		return name, state, fmt.Errorf("failed to apply migrations to project %s: %s, %s", input.ProjectRef, outb.String(), errb.String())
 	}
 
 	return name, state, nil
